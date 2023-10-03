f010289b4e7ad45bf4c3b4e1bd38940119c7dbfa
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2023-10-03 15:04:21 +0300

    Add files via upload

---
+++ uygulama/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMesaj_Click(object sender, EventArgs e)
+        {
+            int yas = 14;
+            if(yas <18)
+            {
+                MessageBox.Show("ehliyet alabilirsiniz");
+            }
+
+            if(yas>=18)
+            {
+                MessageBox.Show("uzgunum biraz daha büyümelisin");
+            }
+        }
+    }
+}
